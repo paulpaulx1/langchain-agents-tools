7ef3089f8b2b6d2236eab0a1d68cda1a3d95cb55
Paul Neenan <paulneenan@Pauls-MBP.lan>
2023-12-10 13:23:22 -0500

    boxen logging"
    dslkfj

--- main.py
+++ main.py
@@ -6,29 +6,38 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
+from tools.report import write_report_tool
+from handlers.chat_model_start_handler import ChatModelStartHandler
 
 load_dotenv()
 
-chat = ChatOpenAI()
+handler = ChatModelStartHandler()
+chat = ChatOpenAI(
+    callbacks=[handler]
+)
 
 tables=list_tables()
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessage(content=(
-            "You are an AI that has access to a SQLite database. \n"
+            "You are an AI that has access to a SQLite database.\n"
             f"The database has tables of: {tables}\n"
-            "Do not make any assumptions about what tables exist"
+            "Do not make any assumptions about what tables exist "
             "or what columns exist. Instead, use the 'describe_tables' function"
-            )),
-        HumanMessagePromptTemplate.from_template('{input}'),
+        )),
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
-tools = [run_query_tool, describe_tables_tool]
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+tools = [run_query_tool, describe_tables_tool, write_report_tool]
+
 agent = OpenAIFunctionsAgent(
     llm=chat,
     prompt=prompt,
@@ -37,8 +46,9 @@ agent = OpenAIFunctionsAgent(
 
 agent_executor = AgentExecutor(
     agent=agent,
-    verbose=True,
-    tools=tools
+    # verbose=True,
+    tools=tools,
+    memory=memory
 )
 
-agent_executor("How many users in the database have shipping addresses?")
+agent_executor("How many orders are there?")
--- tools/sql.py
+++ tools/sql.py
@@ -1,5 +1,8 @@
 import sqlite3
 from langchain.tools import Tool
+from pydantic.v1 import BaseModel
+from typing import List
+
 
 conn = sqlite3.connect("db.sqlite")
 
@@ -17,10 +20,14 @@ def run_sqlite_query(query):
     except sqlite3.OperationalError as err:
         return f"The following error occured: {str(err)}"
 
+class RunQueryArgsSchema(BaseModel):
+    query: str
+
 run_query_tool = Tool.from_function(
     name="run_sqlite_query",
     description="Run a sqlite query.",
-    func=run_sqlite_query
+    func=run_sqlite_query,
+    args_schema=RunQueryArgsSchema
 )
 
 def describe_tables(table_names):
@@ -29,8 +36,11 @@ def describe_tables(table_names):
     rows = c.execute(f"SELECT sql FROM sqlite_master WHERE type='table' and name IN ({tables});")
     return '\n'.join(row[0] for row in rows if row[0] is not None)
 
+class DescribeTablesArgsSchema(BaseModel):
+    tables_names: List[str]
 describe_tables_tool = Tool.from_function(
     name="describe_tables",
     description="Given a list of table names, returns the schema of those tables",
-    func=describe_tables
+    func=describe_tables,
+    arg_schema=DescribeTablesArgsSchema
 )
